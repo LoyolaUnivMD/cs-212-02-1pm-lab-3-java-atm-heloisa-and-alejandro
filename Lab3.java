9747d5904932f94942e81afd4b53ea09361ff7de
heloisa_albuquerque <hralbuquerque@loyola.edu>
2024-02-07 14:14:46 -0500

    In class

--- Lab3.java
+++ Lab3.java
@@ -11,9 +11,47 @@
 import java.util.Scanner;
 
 class Lab3 {
+
+    static double Balance = 212.90;
+
     public static void main(String[] args) {
-       System.out.println("Welcome to Lab 3");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the ATM, what is your name?");
+        String name = input.nextLine();
+
+        System.out.println("It is nice to meet you " + name + " what would you like to do today:");
+        System.out.println("D eposit" + "\n" + "W ithdraw" + "\n" + "B alance" + "\n" + "E xit");
+        String user_choice = input.nextLine();
+
+        while (!user_choice.equals("E")) {
+
+            if (user_choice.equals("D")) {
+                System.out.println("How much would you like to deposit: ");
+                Double deposit_amt = input.nextDouble();
+                Balance += deposit_amt;
+                System.out.println(Balance);
 
 
+            }
+            else if(user_choice.equals("W")) {
+                System.out.println("How much would you like to withdraw: ");
+                Double withdraw_amt = input.nextDouble();
+                while (withdraw_amt > Balance){
+                    System.out.println("Error! Insufficent Funds!");
+                    System.out.println("Please enter a valid amount: ");
+                    withdraw_amt = input.nextDouble();
+
+                }
+                if (withdraw_amt <= Balance){
+                    Balance -= withdraw_amt;
+                    System.out.println(Balance);
+                }
+
+            }
+            System.out.println("What would you like to do now?");
+            System.out.println("D eposit" + "\n" + "W ithdraw" + "\n" +
+                    "B alance" + "\n" + "E xit");
+            user_choice = input.nextLine();
+        }
     }
 }
