a68dd6d99a1b3b11ec27ee62982a42d7c4600df4
Alejandro122133 <156948901+Alejandro122133@users.noreply.github.com>
2024-02-11 18:22:03 -0500

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -26,16 +26,16 @@ class Lab3 {
         System.out.println("It is nice to meet you " + name + " what would you like to do today:");
         System.out.println("D eposit" + "\n" + "W ithdraw" + "\n" + "B alance" + "\n" + "E xit");
         String user_choice = input.nextLine();
-
+//gives user various options
         while (!user_choice.equals("E")) {
-
+//will continue as long as choice does not equal e 
             if (user_choice.equals("D")) {
                 System.out.println("How much would you like to deposit: ");
                 double deposit_amt = input.nextDouble();
                 input.nextLine();
                 Balance += deposit_amt;
                 System.out.println("Your new balance is: " + formatter.format(Balance));
-
+//will add whatever deposited value to Balance
             }
 
             else if(user_choice.equals("W")) {
@@ -44,12 +44,13 @@ class Lab3 {
                 input.nextLine();
                 Balance -= withdraw_amt;
                 System.out.println("Your new balance is: " + formatter.format(Balance));
-
+// will withdraw whatever value user inpouts
                 //Just cant be negative
                 while (withdraw_amt < 0){
                     System.out.println("Error! Insufficent Funds!");
                     System.out.println("Please enter a valid amount: ");
                     withdraw_amt = input.nextDouble();
+                    //error checking to make sure user withdraws correct amount
 
                 }
             }
@@ -57,20 +58,28 @@ class Lab3 {
             else if (user_choice.equals("B")){
                 System.out.println("Here is your current Balance: ");
                 System.out.println(formatter.format(Balance));
+                //will print balance on users command
             }
 
             System.out.println("What would you like to do now?");
             System.out.println("D eposit" + "\n" + "W ithdraw" + "\n" + "B alance" + "\n" + "E xit");
             user_choice = input.nextLine();
+            //after usermakes choice, they can do something else unless they hit E
         }
         if(user_choice.equals("E")){
             System.out.println("Thanks for using the ATM!");
             System.out.println("How would you like your receipt: (P rinted or E mailed) ");
             String receipt_choice = input.nextLine();
-
+//allows user to choose how they want receipt
+            
             if (receipt_choice.equals("E")) {
                 System.out.println("Please remember to check your email");
+                System.out.println("Have a great day!");
 
+            }
+            else if(receipt_choice.equals("P")){
+                System.out.println("*Printing...*");
+                System.out.println("Thanks for using the ATM! Have a great day!");
             }
 
         }
@@ -78,3 +87,4 @@ class Lab3 {
     }
 
 }
+
